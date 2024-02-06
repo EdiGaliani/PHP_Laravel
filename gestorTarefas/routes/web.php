7e401aafabed9de3d706fc128ae8fb90859b9b0e
EdiGaliani <edigaliano@gmail.com>
2024-02-06 17:04:12 -0300

    Project Laravel Created

---
+++ gestorTarefas/routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Main;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    try {
+        DB::connection()->getPdo();
+        echo "Conexão efetuada com sucesso. " . DB::connection()->getDatabaseName();
+    } catch (\Exception $e) {
+        die('Não foi possível ligar à base de dados. Erro:' . $e->getMessage());
+    }
+});
+
+Route::get('/main', [Main::class, 'index']);
